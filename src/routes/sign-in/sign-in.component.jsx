4edb2bc7d8856d013701c0867e97a6b961990411
ayomoses <odukoyaayodeji@gmail.com>
2023-11-06 13:06:40 +0000

    firebase utils added and sign in component

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { signInWithGooglePopup } from '../../utils/firebase/firebase.utils';
+import {
+  signInWithGooglePopup,
+  createUserDocomentFromAuth,
+} from '../../utils/firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
 export const SignIn = () => {
   // any call made to a database is asynchronous
   const logGoogleUser = async () => {
-    const response = await signInWithGooglePopup();
-    console.log(response);
+    const { user } = await signInWithGooglePopup();
+    createUserDocomentFromAuth(user);
   };
   return (
     <div>
--- src/utils/firebase/firebase.utils.jsx
+++ src/utils/firebase/firebase.utils.jsx
@@ -8,6 +8,9 @@ import {
   GoogleAuthProvider,
 } from 'firebase/auth';
 
+// we get firestore and the access to get doc from the document instance, then getDoc and setDoc do not mean what the method names are but getting and setting data from/to firestore. You get Doc with `doc` and set and get data on the doc with getDoc and setDoc. `doc` is a function that takes 3 arguements
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: 'AIzaSyBh4MxyuHHrbPWozMPUV2IuA3WGsEr0sBE',
@@ -32,6 +35,43 @@ provider.setCustomParameters({
   prompt: 'select_account',
 });
 
-// you only need one auth for an app cycle whereas multiple providers 
+// you only need one auth for an app cycle whereas multiple providers
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+// we instantiate firestore and use it to access our DB. This simple instance allows us to tell firestore when we want to set or get data to/from our db
+export const db = getFirestore();
+
+// an async function that takes our authenticated user and sends it to be stored on firestore
+export const createUserDocomentFromAuth = async (userAuth) => {
+  // doc takes 3 arguements.
+  // 1. is our database from firestore
+  // 2. is our collection
+  // 3. is our identifier to identiy the user which comes from the user object when signed in. uid
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  // if user data does not exist
+  // if it exists, the code below will not run since i have inverted with the ! which returns true if it does not exist
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (err) {
+      console.log(err.message, 'There was an error creating the user');
+    }
+  }
+
+  // check if user data exists
+
+  return userDocRef;
+
+  // if it does exist, return user doc ref
+};
