224fd17c08e53bca944a1f255051abf9da988973
ayomoses <odukoyaayodeji@gmail.com>
2024-02-12 19:34:12 +0000

    cart context added to navigation and cart dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,19 @@
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/cart.context';
 import { Button } from '../button/button.component';
 import './cart-dropdown.styles.scss';
 
 export const CartDropdown = () => {
+  const { isCartOpen } = useContext(CartContext);
+
   return (
-    <div className="cart-dropdown-container">
-      <div className="cart-items" />
-      <Button buttonType="">GO TO CHECKOUT</Button>
+    <div>
+      {isCartOpen && (
+        <div className="cart-dropdown-container">
+          <div className="cart-items" />
+          <Button buttonType="">GO TO CHECKOUT</Button>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,13 +1,19 @@
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/cart.context';
+
 import './cart-icon.styles.scss';
 
-// import ShoppingIcon from '../../assets/shopping-bag.svg';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 export const CartIcon = () => {
+  const { setIsCartOpen } = useContext(CartContext);
+  const handleDropDownToggle = () => {
+    setIsCartOpen((prevOpen) => !prevOpen);
+  };
   return (
-    <div className='cart-icon-container'>
-      <ShoppingIcon className='shopping-icon' />
-      <span className='item-count'>0</span>
+    <div className="cart-icon-container" onClick={handleDropDownToggle}>
+      <ShoppingIcon className="shopping-icon" />
+      <span className="item-count">0</span>
     </div>
   );
 };
