df2046fbe7e29df58c5a98208205c36088814a0c
ayomoses <odukoyaayodeji@gmail.com>
2024-02-17 22:56:02 +0000

    refactor toggle of cart set function

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -6,10 +6,12 @@ import './cart-icon.styles.scss';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 export const CartIcon = () => {
-  const { setIsCartOpen } = useContext(CartContext);
-  const handleDropDownToggle = () => {
-    setIsCartOpen((prevOpen) => !prevOpen);
-  };
+  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const handleDropDownToggle = () => setIsCartOpen(!isCartOpen);
+
+  // const handleDropDownToggle = () => {
+  //   setIsCartOpen((prevOpen) => !prevOpen);
+  // };
   return (
     <div className="cart-icon-container" onClick={handleDropDownToggle}>
       <ShoppingIcon className="shopping-icon" />
