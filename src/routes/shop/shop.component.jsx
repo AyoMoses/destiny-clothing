92c7e0d64f0efc80b30a9750894959abc05f13b3
ayomoses <odukoyaayodeji@gmail.com>
2024-05-04 15:46:45 +0100

    category preview and filter first 4 results using filter for first 4 items in category. New category preview component added

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -3,22 +3,18 @@ import { useContext, Fragment } from 'react';
 import { CategoriesContext } from '../../contexts/categories.context';
 import { ProductCard } from '../../components/product-card/product-card.component';
 import './shop.styles.scss';
+import { CategoryPreview } from '../../components/category-preview/category-preview.component';
 
 export const Shop = () => {
   const { categoriesMap } = useContext(CategoriesContext);
   return (
-    <>
-      {Object.keys(categoriesMap).map((title) => (
-        <Fragment key={title}>
-          <h2>{title}</h2>
-
-          <div className="products-container">
-            {categoriesMap[title].map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </Fragment>
-      ))}
-    </>
+    <div className="shop-container">
+      {Object.keys(categoriesMap).map((title) => {
+        const products = categoriesMap[title];
+        return (
+          <CategoryPreview key={title} title={title} products={products} />
+        );
+      })}
+    </div>
   );
 };
