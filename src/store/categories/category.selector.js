d8c0684e4fba1e03f9f69b3703fbaad46ffcafb6
ayomoses <odukoyaayodeji@gmail.com>
2024-07-01 23:04:07 +0100

    create cart folder and cart files

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,10 +1,12 @@
-// pull the data from firebase utils and structure it
+// pull the data from firebase utils and structure it better in our selector file
+
 import { createSelector } from 'reselect';
 // MEMOIZATION TO AVOID RE-RERENDERING
 // slice the categories from the redux categories state
 const selectCategoryReducer = (state) => state.categories;
 
 // THE first is the input and second is what to output
+// pass it into the createSelector so it knows what you have sliced and check if it needs to update
 export const selectCategories = createSelector(
   [selectCategoryReducer],
   (categoriesSlice) => categoriesSlice.categories
