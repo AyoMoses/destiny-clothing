eb652aa6955a806e545bd2e936c7541dfcba79bb
ayomoses <odukoyaayodeji@gmail.com>
2024-07-04 16:01:29 +0100

    persist cart items and black list users from local storage session

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 import { Provider } from 'react-redux';
-import { store } from './store/store';
+import { store, persistor } from './store/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import './index.scss';
 
@@ -14,9 +15,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -6,6 +6,10 @@ import {
 // import { logger } from 'redux-logger';
 import { rootReducer } from './root-reducer';
 
+// local storage
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 const loggerMiddleware = (store) => (next) => (action) => {
   if (!action.type) {
     return next(action);
@@ -20,6 +24,15 @@ const loggerMiddleware = (store) => (next) => (action) => {
   console.log('next state: ', store.getState());
 };
 
+// PERSIST STORAGE ON LOCAL STORAGE WITH REDUX PERSIST
+const persistConfig = {
+  key: 'root',
+  storage: storage, //shorthand is storage
+  blacklist: ['user'], // from the root reducer, we blacklist user from being stored
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 // middlesWares catch action before the hit the store and log them
 // can replace with the actual logger from redux instead of my customer logger ch updates the DOM events better
 const middleWares = [loggerMiddleware];
@@ -28,4 +41,6 @@ const composedEnhancers = compose(applyMiddleware(...middleWares));
 
 // every store needs a root reducer in order to work
 // undefined is the second param to make it easier to test as a default state. It is also optional
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+// export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(persistedReducer, undefined, composedEnhancers);
+export const persistor = persistStore(store);
