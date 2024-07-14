060fc88035ebb6567c59d191562fcab2191772c0
ayomoses <odukoyaayodeji@gmail.com>
2024-07-14 22:02:43 +0100

    integrate stripe payment and test payment

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -10,6 +10,38 @@ export const PaymentForm = () => {
 
   const paymentHandler = async (e) => {
     e.preventDefault();
+
+    const response = await fetch('/.netlify/functions/create-payment-intent', {
+      method: 'post',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({ amount: 10000 }),
+    }).then((res) => res.json());
+
+    console.log(response);
+
+    // const clientSecret = response.paymentIntent.client_secret;
+    const {
+      paymentIntent: { client_secret },
+    } = response;
+
+    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: 'Ayo Oduks',
+        },
+      },
+    });
+
+    if (paymentResult.error) {
+      alert(paymentResult.error);
+    } else {
+      if (paymentResult.paymentIntent.status === 'succeeded') {
+        alert('we succeed payment');
+      }
+    }
   };
 
   // do not continue if the stripe hooks do not return. Hence, a guard clause.
